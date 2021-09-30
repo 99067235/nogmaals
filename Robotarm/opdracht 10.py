6b9e1ae1e7bb78a7a319111f472f6998db234a10
unknown <99067235@mydavinci.nl>
2021-09-30 12:00:40 +0200

    opdracht 10 afgerond

---
+++ Robotarm/opdracht 10.py
@@ -0,0 +1,19 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 10')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.speed = 3
+aantalMoves = 11
+
+for l in range(5):
+    aantalMoves = aantalMoves - 2
+    robotArm.grab()
+    for i in range(0,aantalMoves):
+        robotArm.moveRight()
+    robotArm.drop()
+    for f in range(0,aantalMoves -1):
+        robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
