f0312a8fae92cc1f0aca5cd35b7fe720e931c603
unknown <99067235@mydavinci.nl>
2021-09-30 12:07:17 +0200

    opdracht 10 verbeterd

--- Robotarm/opdracht 10.py
+++ Robotarm/opdracht 10.py
@@ -1,11 +1,8 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 10')
-
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
 aantalMoves = 11
-
 for l in range(5):
     aantalMoves = aantalMoves - 2
     robotArm.grab()
@@ -14,6 +11,5 @@ for l in range(5):
     robotArm.drop()
     for f in range(0,aantalMoves -1):
         robotArm.moveLeft()
-
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
