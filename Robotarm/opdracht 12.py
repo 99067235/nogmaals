22d92406afcfac3ee6f91692a9d028bd9c86bdeb
unknown <99067235@mydavinci.nl>
2021-09-30 15:21:28 +0200

    opdracht 12 afgerond

---
+++ Robotarm/opdracht 12.py
@@ -0,0 +1,27 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 12')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.speed = 4
+aantalMoves = 0
+value = 0
+for f in range(10):
+    robotArm.moveRight()
+for i in range(10):
+    robotArm.grab()
+    color = robotArm.scan()
+    value +=1
+    if color == "red":
+            for g in range(10):
+                robotArm.moveRight()
+            robotArm.drop()
+            for u in range(0,value):
+                robotArm.moveLeft()
+    else:
+        robotArm.drop()
+        robotArm.moveLeft()
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
